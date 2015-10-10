fd29f646c5c86e2ff6301c246392228fd8305222
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2015-10-11 01:15:34 +0900

    Repositoryの基本定義を修正

--- src/main/scala/net/ixias/core/port/adapter/persistence/profile/Repository.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/profile/Repository.scala
@@ -5,28 +5,57 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-package net.ixias.core.port.adapter.persistence
+package net.ixias.core
+package port.adapter.persistence.profile
 
 import com.typesafe.config.{ Config, ConfigFactory }
 import net.ixias.core.domain.model.{ Identity, Entity }
+import net.ixias.core.port.adapter.persistence.lifted._
+import net.ixias.core.port.adapter.persistence.backend.DatabaseComponent
 
 /**
  * The basic functionality that has to be implemented by all repositories.
  */
-trait BasicProfile[ID <: Identity[_], E <: Entity[ID]]
-    extends RepositoryActionComponent[ID, E] { repository: BasicRepository[ID, E] =>
-  trait API {
-  }
-  val api: API = new API {}
+trait BasicProfile[K <: Identity[_], V <: Entity[K]] extends BasicActionComponent[K, V] {
+  repository: BasicRepository[K, V] =>
+
+  // --[ TypeDefs ]-------------------------------------------------------------
+  /** The identity type of entity */
+  type Id = K
+  /** The entity type of managed by this profile */
+  type Entity = V
+  /** The back-end type required by this profile */
+  type Backend <: DatabaseComponent
+
+  // --[ Properties ]-----------------------------------------------------------
+  /** The back-end implementation for this profile */
+  // val backend: Backend
+
+  // trait ExtensionMethodConversions {
+  //   /** For extending `Future` methods. */
+  //   @inline implicit def extFutureOps[A](a: Future[A]) = new FutureOps[A](a)
+  // }
+
+  // trait API extends ExtensionMethodConversions {
+    /** Contains an error messsage when a command fails validation. */
+    // type Error = String
+    /** Used to validate commands received by the system that act on the adapter. */
+    // type ValidationNel[A] = scalaz.ValidationNel[Error, A]
+ // }
+
+  /** The API for using the utility methods with a single import statement.
+    * This provides the repository's implicits, the Database connections,
+    * and commonly types and objects. */
+  // val api: API
 }
 
-trait RepositoryActionComponent[ID <: Identity[_], E <: Entity[ID]] {
-  repository: BasicRepository[ID, E] =>
+trait BasicActionComponent[K <: Identity[_], V <: Entity[K]] {
+  repository: BasicRepository[K, V] =>
 }
 
-trait BasicRepository[ID <: Identity[_], E <: Entity[ID]] extends BasicProfile[ID, E] {
+trait BasicRepository[K <: Identity[_], V <: Entity[K]] extends BasicProfile[K, V] {
   /** The external interface of this repository which defines the API. */
-  val profile: BasicProfile[ID, E] = this
+  val profile: BasicProfile[K, V] = this
 
   /** The configuration for this repository */
   final lazy val config: Config = loadRepositoryConfig
