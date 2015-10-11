5da0e81d217c661a5330422329644c06aa9943d7
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2015-10-11 11:05:00 +0900

    バックエンド コンポーネントの雛形を作成

--- src/main/scala/net/ixias/core/port/adapter/persistence/backend/DatabaseComponent.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/backend/DatabaseComponent.scala
@@ -5,8 +5,40 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-package net.ixias.core
-package port.adapter.persistence.backend
+package net.ixias
+package core
+package port.adapter.persistence
+package backend
 
-trait DatabaseComponent {
+import action.IOActionContext
+
+trait DatabaseComponent { self =>
+
+  // --[ TypeDefs ]-------------------------------------------------------------
+  type This >: this.type <: DatabaseComponent
+
+  /** The type of database source config used by this backend. */
+  type DatabaseSouceConfig <: DatabaseSouceConfigDef
+  /** The type of the database souce config factory used by this backend. */
+  type DatabaseSouceConfigFactory <: DatabaseSouceConfigFactoryDef
+  /** The type of the context used for running Database Actions */
+  type Context >: Null <: DatabaseActionContext
+
+  // --[ Properties ]-----------------------------------------------------------
+  /** The database factory */
+  val DatabaseSouceConfig: DatabaseSouceConfigFactory
+
+  // --[ DatabaseSouceConfigDef ] ----------------------------------------------
+  /** A database souce config instance to which connections can be created. */
+  trait DatabaseSouceConfigDef extends Serializable { this: DatabaseSouceConfig =>
+  }
+
+  // --[ DatabaseSouceConfigFactoryDef ] ---------------------------------------
+  /** The database souce config factory */
+  trait DatabaseSouceConfigFactoryDef { this: DatabaseSouceConfigFactory =>
+  }
+
+  /** The context object passed to database actions by the repository. */
+  trait DatabaseActionContext extends IOActionContext {
+  }
 }
