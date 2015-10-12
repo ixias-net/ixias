f57515c87b1549ed1ede859cbb92964692fa556a
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2015-10-12 23:39:28 +0900

    Future -> ValidationNelへの暗黙変換関数を追加

---
+++ src/main/scala/net/ixias/core/port/adapter/persistence/lifted/ExtensionMethods.scala
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core
+package port.adapter.persistence
+package lifted
+
+import scala.language.implicitConversions
+
+trait ExtensionMethodConversions extends ToFutureOps
---
+++ src/main/scala/net/ixias/core/port/adapter/persistence/lifted/FutureOps.scala
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core
+package port.adapter.persistence
+package lifted
+
+import scalaz._
+import scalaz.Scalaz._
+import scala.concurrent.{ Future, Await }
+import scala.concurrent.duration.Duration
+import scala.language.implicitConversions
+import action.IOAction
+
+final class FutureOps[A](val self: Future[A]) {
+  def await[A1](implicit convert: A => A1): IOAction#ValidationNel[A1] = {
+    Await.ready(self, Duration.Inf)
+    self.value.get match {
+      case scala.util.Success(v) => convert(v).successNel
+      case scala.util.Failure(t) => (t.getMessage()).failureNel
+    }
+  }
+}
+
+trait ToFutureOps {
+  implicit def ToFutureOps[A](a: Future[A]) = new FutureOps(a)
+}
