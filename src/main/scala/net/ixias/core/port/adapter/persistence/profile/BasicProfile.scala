9e0233a2d10452a4de75644f3807629ee8b9a499
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2015-10-12 23:40:05 +0900

    リポジトリの振る舞いを定義した基底クラスを追加

---
+++ src/main/scala/net/ixias/core/port/adapter/persistence/profile/BasicProfile.scala
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core
+package port.adapter.persistence
+package profile
+
+import action.IOAction
+import backend.DatabaseComponent
+import lifted.ExtensionMethodConversions
+import com.typesafe.config.{ Config, ConfigFactory }
+
+/**
+ * The basic functionality that has to be implemented by all repositories.
+ */
+trait BasicProfile extends BasicActionComponent {
+
+  // --[ TypeDefs ]-------------------------------------------------------------
+  /** The identity type of entity */
+  type Id      <: domain.model.Identity[_]
+  /** The entity type of managed by this profile */
+  type Entity  <: domain.model.Entity[Id]
+  /** The back-end type required by this profile */
+  type Backend <: DatabaseComponent
+
+  // --[ Properties ]-----------------------------------------------------------
+  /** The external interface of this repository which defines the API. */
+  val profile: BasicProfile = this
+
+  /** The back-end implementation for this profile */
+  val backend: Backend
+
+  /** The configuration for persistence */
+  final lazy val config: Config = loadPersistenceConfig
+
+  // --[ Methods ]--------------------------------------------------------------
+  /** Load the configuration for this repository. This can be overridden in
+    * user-defined repository subclasses to load different configurations. */
+  protected[this] def loadPersistenceConfig: Config = {
+    ConfigFactory.load()
+  }
+
+  /** The API for using the utility methods with a single import statement.
+    * This provides the repository's implicits, the Database connections,
+    * and commonly types and objects. */
+  trait API extends ExtensionMethodConversions {
+    type Error            = IOAction#Error
+    type ValidationNel[A] = IOAction#ValidationNel[A]
+  }
+  val api: API
+}
+
+trait BasicActionComponent { profile: BasicProfile =>
+}
---
+++ src/main/scala/net/ixias/core/port/adapter/persistence/profile/RepositoryProfile.scala
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core
+package port.adapter.persistence
+package profile
+
+import domain.model.{ Identity, Entity }
+import action.EntityIOAction
+import backend.RepositoryBackend
+import com.typesafe.config.{ Config, ConfigFactory }
+
+/**
+ * The basic functionality that has to be implemented by all repositories.
+ */
+trait RepositoryProfile[K <: Identity[_], V <: Entity[K]]
+    extends BasicProfile with RepositoryActionComponent[K, V] {
+
+  // --[ TypeDefs ]-------------------------------------------------------------
+  /** The identity type of entity */
+  type Id      = K
+  /** The entity type of managed by this profile */
+  type Entity  = V
+  /** The back-end type required by this profile */
+  type Backend = RepositoryBackend
+
+  // --[ Properties ]-----------------------------------------------------------
+  /** The back-end implementation for this profile */
+  val backend: Backend = new RepositoryBackend {}
+
+  // --[ Methods ]--------------------------------------------------------------
+  /** The API for using the utility methods with a single import statement.
+    * This provides the repository's implicits, the Database connections,
+    * and commonly types and objects. */
+  trait API extends super.API {
+  }
+  val api: API = new API {}
+}
+
+trait RepositoryActionComponent[K <: Identity[_], V <: Entity[K]]
+    extends BasicActionComponent with EntityIOAction[K, V] {
+  profile: RepositoryProfile[K, V] =>
+}
