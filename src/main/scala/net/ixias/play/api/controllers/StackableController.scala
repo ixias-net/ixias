6ee0d741464ea8b723015ac49603477b4357c01c
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2015-10-27 02:08:07 +0900

    AOP Invocation の proceed methodに近い実装をできるようなロジックを実装

--- src/main/scala/net/ixias/play/api/controllers/StackableController.scala
+++ src/main/scala/net/ixias/play/api/controllers/StackableController.scala
@@ -17,9 +17,31 @@ import scala.util.control.{NonFatal, ControlThrowable}
 trait StackableController { self: Controller =>
   import StackableRequest._
 
-  case class StackAction(params: Attribute[_]*) extends ActionBuilder[StackableRequest] {
-    def invokeBlock[A](req: Request[A], block: (StackableRequest[A]) => Future[Result]): Future[Result] = ???
+  /** Proceed with the next advice or target method invocation */
+  def proceed[A](request: StackableRequest[A])(f: StackableRequest[A] => Future[Result]): Future[Result] = f(request)
+
+  /** Custom action builders */
+  sealed case class StackAction(params: Attribute[_]*) extends ActionBuilder[StackableRequest] {
+    def invokeBlock[A](request: Request[A], block: (StackableRequest[A]) => Future[Result]): Future[Result] = {
+      val requestExt = new StackableRequest(request, new TrieMap[StackableRequest.AttributeKey[_], Any] ++= params.map(_.toTuple))
+      try {
+        implicit val ctx = createStackActionExecutionContext(requestExt)
+        proceed(requestExt)(block) andThen {
+          case Success(p) => cleanupOnSuccess(requestExt, Some(p))
+          case Failure(e) => cleanupOnFailure(requestExt, e)
+        }
+      } catch {
+        case e: ControlThrowable => cleanupOnSuccess(requestExt, None); throw e
+        case NonFatal(e)         => cleanupOnFailure(requestExt, e);    throw e
+      }
+    }
   }
+
+  // --[ ExecutionContext ] ----------------------------------------------------
+  protected object ExecutionContextKey extends StackableRequest.AttributeKey[ExecutionContext]
+  protected def createStackActionExecutionContext(implicit request: StackableRequest[_]): ExecutionContext =
+    request.get(ExecutionContextKey).getOrElse(
+      _root_.play.api.libs.concurrent.Execution.defaultContext)
 
   // --[ Constructs an Action ] ------------------------------------------------
   /** Constructs an `Action` with default content, and no request parameter. */
@@ -59,5 +81,5 @@ trait StackableController { self: Controller =>
   def cleanupOnSuccess[A](request: StackableRequest[A], result: Option[Result]): Unit = cleanupOnSuccess(request)
 
   /** This method will be called bu StackAction when invokeBlock failed. */
-  def cleanupOnFailure[A](request: StackableRequest[A]): Unit = ()
+  def cleanupOnFailure[A](request: StackableRequest[A], e: Throwable): Unit = ()
 }
