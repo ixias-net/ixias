eb7890f1094e260b952fa6464e2f9817b7756425
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2015-10-27 00:28:38 +0900

    play.api.mvc.Controllerを単純に継承したもの

---
+++ src/main/scala/net/ixias/play/api/controllers/Controller.scala
@@ -0,0 +1,11 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package play.api.controllers
+
+trait Controller extends _root_.play.api.mvc.Controller
