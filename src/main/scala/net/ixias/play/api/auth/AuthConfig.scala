af6b368ac5630ee32946bc8739a1236df1319bda
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2015-10-27 20:02:30 +0900

    認証設定ファイルのCallbackMethodsに説明を追加

--- src/main/scala/net/ixias/play/api/auth/AuthConfig.scala
+++ src/main/scala/net/ixias/play/api/auth/AuthConfig.scala
@@ -47,12 +47,31 @@ trait AuthConfig { config =>
   lazy val datastore: WrappedContainer[Id] = WrappedContainer(CacheContainer[Id])
 
   // --[ Methods ]--------------------------------------------------------------
-  def resolve(id: Id)(implicit context: ExecutionContext): Future[Option[User]]
+  /** Resolve user by specified user-id. */
+  def resolveUser(id: Id)(implicit context: ExecutionContext): Future[Option[User]]
+
+  /** Verifies what user are authorized to do. */
   def authorize(user: User, authority: Authority)(implicit context: ExecutionContext): Future[Boolean]
 
   // --[ Methods ]--------------------------------------------------------------
-  def       loginSucceeded(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
-  def      logoutSucceeded(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
-  def authenticationFailed(request: RequestHeader)(implicit context: ExecutionContext): Future[Result]
-  def  authorizationFailed(request: RequestHeader, user: User, authority: Option[Authority])(implicit context: ExecutionContext): Future[Result]
+  /** Invoked after a user has successfully logged in. */
+  def loginSucceeded(request: RequestHeader)
+    (implicit context: ExecutionContext): Future[Result]
+
+  /** Invoked after a user has successfully logged out. */
+  def logoutSucceeded(request: RequestHeader)
+    (implicit context: ExecutionContext): Future[Result]
+
+  /** Invoked if authentication failed with the credentials provided.
+    * This should only be called where an authentication attempt has truly failed */
+  def authenticationFailed(request: RequestHeader)
+    (implicit context: ExecutionContext): Future[Result]
+
+  /** Invoked if authorization failed.
+    * Authorization is the process of allowing an authenticated users to
+    * access the resources by checking whether the user has access rights to the system.
+    * Authorization helps you to control access rights by granting or
+    * denying specific permissions to an authenticated user. */
+  def authorizationFailed(request: RequestHeader, user: User, authority: Option[Authority])
+    (implicit context: ExecutionContext): Future[Result]
 }
