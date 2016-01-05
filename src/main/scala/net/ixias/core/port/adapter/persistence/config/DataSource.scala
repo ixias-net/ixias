0c3735eaefc6636f9e15e9daf591411e4f986d57
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-01-05 15:38:46 +0900

    DNSからDataSourceオブジェクトを構築する処理

--- src/main/scala/net/ixias/core/port/adapter/persistence/config/DataSource.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/config/DataSource.scala
@@ -9,64 +9,21 @@ package net.ixias
 package core.port.adapter.persistence.config
 
 import scala.util.{ Try, Success, Failure }
+import core.util.EnumOf
 import core.port.adapter.persistence.backend.Protocol
 
 /** The data source object. */
 case class DataSource(
-  private var path:     String,
-  private var protocol: Protocol       = Protocol.TCP,
-  private var socket:   Option[String] = None,
-  private var hostspec: Option[String] = None,
-  private var port:     Option[Int]    = None,
-  private var database: Option[String] = None,
-  private var user:     Option[String] = None,
-  private var password: Option[String] = None,
-  private var options:  Map[String, String] = Map.empty
+  var path:     String,
+  var protocol: Option[Protocol] = None,
+  var socket:   Option[String]   = None,
+  var hostspec: Option[String]   = None,
+  var port:     Option[Int]      = None,
+  var database: Option[String]   = None,
+  var user:     Option[String]   = None,
+  var password: Option[String]   = None,
+  var options:  Map[String, String] = Map.empty
 ) {
-
-  // The Path of typesafe config
-  def getPath(): String              = this.path
-  def setPath(v: String)             = this.path = v
-
-  // Protocol
-  def getProtocol(): Protocol        = this.protocol
-  def setProtocol(v: Protocol)       = this.protocol = v
-
-  // The path of socket
-  def getSocket(): Option[String]    = this.socket
-  def setSocket(v: Option[String])   = this.socket = v
-  def setSocket(v: String)           = this.socket = Some(v)
-
-  // Hostspec
-  def getHostSpec(): Option[String]  = this.hostspec
-  def setHostSpec(v: Option[String]) = this.hostspec = v
-  def setHostSpec(v: String)         = this.hostspec = Some(v)
-
-  // Port
-  def getPort(): Option[Int]         = this.port
-  def setPort(v: Option[Int])        = this.port = v
-  def setPort(v: Int)                = this.port = Some(v)
-
-  // Database
-  def getDatabase(): Option[String]  = this.database
-  def setDatabase(v: Option[String]) = this.database = v
-  def setDatabase(v: String)         = this.database = Some(v)
-
-  // User
-  def getUser(): Option[String]      = this.user
-  def setUser(v: Option[String])     = this.user = v
-  def setUser(v: String)             = this.user = Some(v)
-
-  // Password
-  def getPassword(): Option[String]  = this.password
-  def setPassword(v: Option[String]) = this.password = v
-  def setPassword(v: String)         = this.password = Some(v)
-
-  // Options
-  def getOptions(): Map[String, String]  = this.options
-  def  addOption(v: (String, String))    = this.options += v
-  def setOptions(v: Map[String, String]) = this.options  = v
-
   override def toString =
     s"""|${this.getClass.getSimpleName}: {
         |  path:     $path,
@@ -83,5 +40,70 @@ case class DataSource(
 
 /** The factory for data source object. */
 object DataSource {
-  def forName(name: String): Try[DataSource] = ???
+  /** build a `DataSource` object by DSN as string.
+    * DNS example :
+    *   - slick.db://master/database1
+    *   - slick.db://user:password@master/database1
+    *   - slick.db://user:password@127.0.0.1:103306/database1
+    *   - slick.db://user:password@udp(127.0.0.1:103306)/database1
+    *   - slick.db://user:password@unix(/var/tmp/mysql.sock)
+    */
+  def forName(name: String): Try[DataSource] = {
+    // Create a data souce object.
+    var (source, tail) = name.indexOf("://") match {
+      case -1  => throw new Exception(s"""Dose not match the DSN format. ($name)""")
+      case pos => (DataSource(path = name.substring(0, pos)), name.substring(pos + 3))
+    }
+    // PART -> user:password.
+    tail.indexOf("@") match { case -1 => case pos =>
+      val tok = tail.substring(0, pos)
+      tok.indexOf(":") match {
+        case -1  => source.user     = Some(tok)
+        case pos => source.user     = Some(tok.substring(0, pos))
+                    source.password = Some(tok.substring(pos + 1))
+      }
+      tail = tail.substring(pos + 1)
+    }
+    // PART(left)  -> hostspec or proto(proto_opts)
+    // PART(right) -> database and options
+    var (left, right, closed, separator) = ("", "", false, false)
+    tail.map(_ match {
+      case '/' if !closed => separator = true
+      case c => {
+        if (c == '(') closed = true
+        if (c == ')') closed = false
+        if (!separator) left += c else right += c
+      }
+    })
+    // PART(left) -> hostspec or proto(proto_opts)
+    if (0 < left.length) {
+      val regex = """^(.+?)\((.*)\)$""".r
+      val (hostspec, protocol) = left match {
+        case regex("udp",  h) => (h, Protocol.UDP)
+        case regex("unix", h) => (h, Protocol.Unix)
+        case regex(_,      h) => (h, Protocol.Unknown)
+        case h                => (h, Protocol.TCP)
+      }
+      source.protocol = Some(protocol)
+      source.protocol.map(_ match {
+        case Protocol.Unix => source.socket = Some(hostspec)
+        case _ => hostspec.indexOf(":") match {
+          case  -1 => source.hostspec = Some(hostspec)
+          case pos => source.hostspec = Some(hostspec.substring(0, pos))
+                      source.port     = Some(hostspec.substring(pos + 1).toInt)
+        }
+      })
+    }
+    // PART(right) -> database and options
+    if (0 < right.length) {
+      val regex = """^(.+)=(.+)$""".r
+      right.lastIndexOf("?") match {
+        case  -1 => source.database = Some(right)
+        case pos => source.database = Some(right.substring(0, pos))
+          right.substring(pos + 1).split("&").map {
+            case regex(k, v) => source.options += (k -> v) }
+      }
+    }
+    Success(source)
+  }
 }
---
+++ src/test/scala/net/ixias/core/port/adapter/persistence/config/DataSourceSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core.port.adapter.persistence.config
+
+import org.specs2.mutable.Specification
+
+class DataSourceSpec extends Specification {
+  "DataSource" should {
+    "forName" in {
+      val exists = 1
+      // val source1 = DataSource.forName("slick.db://slave/kidsna_udb")
+      // val source2 = DataSource.forName("slick.db://user:password@slave/kidsna_udb")
+      // val source3 = DataSource.forName("slick.db://user:password@slave:103306/kidsna_udb")
+      // val source4 = DataSource.forName("slick.db://user:password@slave")
+      // val source5 = DataSource.forName("slick.db://user:password@udp(127.0.0.1:9000)/kidsna_udb")
+      // val source6 = DataSource.forName("slick.db://user:password@unix(/var/tmp/mysql.sock)/kidsna_udb?var1=xxxx&var2=zzzz")
+      // val source7 = DataSource.forName("slick.db://user:password@unix(/var/tmp/mysql.sock)")
+      // println(source1)
+      // println(source2)
+      // println(source3)
+      // println(source4)
+      // println(source5)
+      // println(source6)
+      // println(source7)
+      exists must_== 1
+    }
+  }
+}
