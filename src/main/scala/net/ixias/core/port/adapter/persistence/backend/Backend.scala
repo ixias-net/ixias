a5df296c64157461f1ac8d31d5a3fcbc84c9f2fd
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-01-07 16:31:50 +0900

    DSN Structの定義ファイルを変更

--- src/main/scala/net/ixias/core/port/adapter/persistence/backend/Backend.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/backend/Backend.scala
@@ -8,29 +8,6 @@
 package net.ixias
 package core.port.adapter.persistence.backend
 
-import scala.language.implicitConversions
-
 /** Backend for the basic database and session handling features. */
 trait Backend extends DataSource
 
-/** The DSN structure. */
-case class DataSourceName(
-  val path:     String,
-  val hostspec: String,
-  val database: String
-)
-
-/** Conpanion object */
-object DataSourceName {
-
-  /** The synatx format for DSN */
-  val SYNTAX_DATA_SOURCE_NAME = """^([.\w]+)://(\w+?)/(\w+)$""".r
-
-  object Implicits {
-    /** Convert to structure data from dsn as string */
-    implicit def convertToStruct(name: String): DataSourceName = name match {
-      case SYNTAX_DATA_SOURCE_NAME(p1, p2, p3) => DataSourceName(p1, p2, p3)
-      case _ => throw new Exception(s"""Dose not match the DSN format. ($name)""")
-    }
-  }
-}
--- src/main/scala/net/ixias/core/port/adapter/persistence/backend/DataSource.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/backend/DataSource.scala
@@ -9,6 +9,7 @@ package net.ixias
 package core.port.adapter.persistence.backend
 
 import scala.util.Try
+import scala.language.implicitConversions
 import core.port.adapter.persistence.io.EntityIOActionContext
 
 trait DataSource {
@@ -35,5 +36,27 @@ trait DataSource {
   trait DataSourceFactoryDef {
     /** Load a configuration for persistent database. */
     def forDSN(name: String)(implicit ctx: Context): Try[DataSource]
+  }
+}
+
+/** The DSN structure. */
+case class DataSourceName(
+  val path:     String,
+  val hostspec: String,
+  val database: String
+)
+
+/** Conpanion object */
+object DataSourceName {
+
+  /** The synatx format for DSN */
+  val SYNTAX_DATA_SOURCE_NAME = """^([.\w]+)://(\w+?)/(\w+)$""".r
+
+  object Implicits {
+    /** Convert to structure data from dsn as string */
+    implicit def convertToStruct(name: String): DataSourceName = name match {
+      case SYNTAX_DATA_SOURCE_NAME(p1, p2, p3) => DataSourceName(p1, p2, p3)
+      case _ => throw new Exception(s"""Dose not match the DSN format. ($name)""")
+    }
   }
 }
