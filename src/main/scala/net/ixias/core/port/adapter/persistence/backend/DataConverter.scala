3e65450aa005095dda247395687dc116e027f290
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-02-07 12:23:54 +0900

    Slick <-> Domain Model の 変換器の雛形を追加

---
+++ src/main/scala/net/ixias/core/port/adapter/persistence/backend/DataConverter.scala
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package net.ixias
+package core.port.adapter.persistence.backend
+
+/**
+ * Represents a type class that needs to be implemented
+ * for conversion to work.
+ */
+trait DataConverter[T, R] {
+  
+}
