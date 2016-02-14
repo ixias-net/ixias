7f6b60c3db926e4c4c1e8a72a240b31b76bef5b5
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-02-14 19:56:13 +0900

    SlickProfileの実装のためのテストコードを追加

--- src/test/scala/net/ixias/core/port/adapter/persistence/model/TableSpec.scala
+++ src/test/scala/net/ixias/core/port/adapter/persistence/model/TableSpec.scala
@@ -13,65 +13,36 @@ import slick.driver.JdbcProfile
 import core.domain.model._
 import org.specs2.mutable.Specification
 
-// 定義
-//~~~~~~
-case class User (
-  val id:        Identity[Long],
-  val name:      Option[String],
-  val email:     Option[String],
-  val updatedAt: DateTime = new DateTime,
-  val createdAt: DateTime = new DateTime
-) extends Entity[Long]
-
-/** レコード定義 */
-case class UserTableRecord (
-  val id:        Long,
-  val name:      Option[String],
-  val email:     Option[String],
-  val updatedAt: DateTime = new DateTime,
-  val createdAt: DateTime = new DateTime
-)
-
 /** テーブル定義 */
-trait UserTable[P <: JdbcProfile] extends Table[UserTableRecord, P] {
+case class UserTable[P <: JdbcProfile](val driver: P) extends Table[P] { self =>
+  import api._
 
-  lazy val query = new TableQuery
+  /** DNS定義 */
+  lazy val dsn = Map(
+    "master" -> DataSourceName("ixais.db.slick://master/test"),
+    "slave"  -> DataSourceName("ixais.db.slick://slave/test")
+  )
+
+  /** レコード定義 */
+  case class Record (
+    val id:        Long,
+    val name:      Option[String],
+    val email:     Option[String],
+    val updatedAt: DateTime = new DateTime,
+    val createdAt: DateTime = new DateTime
+  )
 
   /** テーブル定義 */
   class Table(tag: Tag) extends BasicTable(tag, "user") {
-    import api._
     def id        = column[Long]           ("uid",        O.AsciiChar8, O.PrimaryKey)
     def name      = column[Option[String]] ("name",       O.AsciiChar8, O.PrimaryKey)
     def email     = column[Option[String]] ("email",      O.AsciiChar8, O.PrimaryKey)
     def updatedAt = column[DateTime]       ("updated_at", O.TsCurrent)
     def createdAt = column[DateTime]       ("created_at", O.Ts)
-    def * = (id, name, email, updatedAt, createdAt) <> (UserTableRecord.tupled, UserTableRecord.unapply)
+    def * = (id, name, email, updatedAt, createdAt) <> (Record.tupled, Record.unapply)
   }
 
   /** クエリー定義 */
-  class TableQuery extends BasicTableQuery(new Table(_))
-}
-
-/** 変換定義 */
-// テスト
-//~~~~~~~~
-class TableSpec extends Specification {
-  def unlift[A, B](f: A => Option[B]): A => B = Function.unlift(f)
-
-  implicit object UserConv extends Converter[UserTableRecord, User] {
-    def convert(o: UserTableRecord): User =
-      new User(SomeId(o.id), o.name, o.email, o.updatedAt, o.createdAt)
-  }
-
-  "Table" should {
-    "declare" in {
-      val exists = 1
-      val user = UserTableRecord(1001, Some("name"), Some("name@nextbeat.net"))
-
-      // println(userWrites.writes(user))
-      // println(unlift(UserTableRecord.unapply)(user))
-
-      exists must_== 1
-    }
-  }
+  class Query extends BasicQuery(new Table(_))
+  lazy val query = new Query
 }
