a633cba02a47f16308c390db3cff5648cbbced15
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-02-17 12:40:48 +0900

    テーブル作成と削除のためのサポートツール関数を追加

--- src/main/scala/net/ixias/core/port/adapter/persistence/util/SlickTool.scala
+++ src/main/scala/net/ixias/core/port/adapter/persistence/util/SlickTool.scala
@@ -11,20 +11,32 @@ package core.port.adapter.persistence.util
 import slick.driver.JdbcProfile
 import scala.concurrent.Future
 import core.port.adapter.persistence.model.{ Table, Converter }
-import core.port.adapter.persistence.action.SlickDBActionProvider
+import core.port.adapter.persistence.action.{ SlickDBActionProvider, SlickRunDBActionProvider }
 
 /**
  * The utility tool to manage database with using Slick library.
  */
 case class SlickTool[P <: JdbcProfile](implicit val driver: P)
-    extends SlickDBActionProvider[P]
+    extends SlickDBActionProvider[P] with SlickRunDBActionProvider[P]
 {
-  /** Show create table SQL statements */
+  /** Show create table SQL statements. */
   def showCreateTable[T <: Table[_, P]](table: T)(implicit conv: Converter[_, _]): Future[Unit] = {
     import driver.api._
     SlickDBAction(table) { case (_, slick) =>
       slick.schema.create.statements.foreach(println)
       Future.successful(Unit)
     }
+  }
+
+  /** Create table. */
+  def createTable[T <: Table[_, P]](table: T)(implicit conv: Converter[_, _]): Future[Unit] = {
+    import driver.api._
+    SlickRunDBAction(table)(_.schema.create)
+  }
+
+  /** Drop table. */
+  def dropTable[T <: Table[_, P]](table: T)(implicit conv: Converter[_, _]): Future[Unit] = {
+    import driver.api._
+    SlickRunDBAction(table)(_.schema.drop)
   }
 }
