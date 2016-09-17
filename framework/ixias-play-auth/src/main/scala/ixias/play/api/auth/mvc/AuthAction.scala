431694d7612d4b7a0500f4e863b927ecf9d36d0c
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2016-09-18 00:06:03 +0900

    StackActionのリファクタリング

--- framework/ixias-play-auth/src/main/scala/ixias/play/api/auth/mvc/AuthAction.scala
+++ framework/ixias-play-auth/src/main/scala/ixias/play/api/auth/mvc/AuthAction.scala
@@ -14,11 +14,15 @@ import ixias.play.api.mvc.{ StackActionRequest, StackActionBuilder }
 /**
  * Provides helpers for creating `Action` values.
  */
-trait AuthActionBuilder extends StackActionBuilder {
+trait AuthActionBuilder extends StackActionBuilder[StackActionRequest] {
   self =>
 
   /** The key of attribute for containing required authority roles. */
   case object AuthProfileKey extends StackActionRequest.AttributeKey[AuthProfile]
+
+  // // --[ Methods ] -------------------------------------------------------------
+  final def apply(auth: AuthProfile): StackActionBuilder[StackActionRequest] =
+    apply(AuthProfileKey -> auth)
 
   // --[ Methods ] -------------------------------------------------------------
   /**
@@ -29,31 +33,4 @@ trait AuthActionBuilder extends StackActionBuilder {
       case Some(auth) => block(auth)
       case None       => Future.successful(Results.InternalServerError)
     }
-
-  // --[ Methods ] -------------------------------------------------------------
-  /**
-   * Constructs an `Action` with default content, and no request parameter.
-   */
-  final def apply(auth: AuthProfile)(block: => Result): Action[AnyContent] =
-    apply(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent), AuthProfileKey -> auth)(_ => block)
-
-  /**
-   * Constructs an `Action` with default content
-   */
-  final def apply(auth: AuthProfile)(block: StackActionRequest[AnyContent] => Result): Action[AnyContent] =
-    apply(BodyParsers.parse.default, AuthProfileKey -> auth)(block)
-
-  // --[ Methods ] -------------------------------------------------------------
-  /**
-   * Constructs an `Action` that returns a future of a result,
-   * with default content, and no request parameter.
-   */
-  final def async(auth: AuthProfile)(block: => Future[Result]): Action[AnyContent] =
-    async(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent), AuthProfileKey -> auth)(_ => block)
-
-  /**
-   * Constructs an `Action` that returns a future of a result, with default content
-   */
-  final def async(auth: AuthProfile)(block: StackActionRequest[AnyContent] => Future[Result]): Action[AnyContent] =
-    async(BodyParsers.parse.default, AuthProfileKey -> auth)(block)
 }
--- framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/StackAction.scala
+++ framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/StackAction.scala
@@ -39,6 +39,14 @@ case class StackActionRequest[A](
     attributes.put(key, value)
     this
   }
+
+  /**
+   * Store an attributes
+   */
+  def ++=[B](tail: TrieMap[AttributeKey[_], Any]): StackActionRequest[A] = {
+    attributes ++= tail
+    this
+  }
 }
 
 // The declaration for request's attribute.
@@ -60,12 +68,10 @@ object StackActionRequest {
   }
 }
 
-// Statck Action Function
-//~~~~~~~~~~~~~~~~~~~~~~~~~
 /**
  * A builder for generic Actions that generalizes over the type of requests.
  */
-trait StackActionFunction extends ActionFunction[StackActionRequest, StackActionRequest] {
+sealed trait StackActionInjector {
 
   /**
    * Get an instance of the given class from the injector.
@@ -73,7 +79,7 @@ trait StackActionFunction extends ActionFunction[StackActionRequest, StackAction
   def instanceOf[T: ClassTag]: Future[T] =
     Future(Play.routesCompilerMaybeApplication.map(
       _.injector.instanceOf[T]
-    ).get)(executionContext)
+    ).get)(play.api.libs.iteratee.Execution.trampoline)
 
   /**
    * Get an instance of the given class from the injector.
@@ -81,7 +87,7 @@ trait StackActionFunction extends ActionFunction[StackActionRequest, StackAction
   def instanceOf[T](clazz: Class[T]): Future[T] =
     Future(Play.routesCompilerMaybeApplication.map(
       _.injector.instanceOf[T](clazz)
-    ).get)(executionContext)
+    ).get)(play.api.libs.iteratee.Execution.trampoline)
 
   /**
    * Get an instance bound to the given binding key.
@@ -89,13 +95,15 @@ trait StackActionFunction extends ActionFunction[StackActionRequest, StackAction
   def instanceOf[T](key: BindingKey[T]): Future[T] =
     Future(Play.routesCompilerMaybeApplication.map(
       _.injector.instanceOf[T](key)
-    ).get)(executionContext)
+    ).get)(play.api.libs.iteratee.Execution.trampoline)
 }
 
+// Statck Action Function
+//~~~~~~~~~~~~~~~~~~~~~~~~~
 /**
  * Provides helpers for creating `Action` values.
  */
-trait StackActionBuilder extends StackActionFunction {
+trait StackActionBuilder[+R[_]] extends ActionFunction[StackActionRequest, R] with StackActionInjector {
   self =>
 
   // --[ Methods ] -------------------------------------------------------------
@@ -106,28 +114,16 @@ trait StackActionBuilder extends StackActionFunction {
     apply(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent))(_ => block)
 
   /**
-   * Constructs an `Action` with default content.
-   */
-  final def apply(params: Attribute[_]*)(block: => Result): Action[AnyContent] =
-    apply(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent), params: _*)(_ => block)
-
-  /**
    * Constructs an `Action` with default content
    */
-  final def apply(block: StackActionRequest[AnyContent] => Result): Action[AnyContent] =
+  final def apply(block: R[AnyContent] => Result): Action[AnyContent] =
     apply(BodyParsers.parse.default)(block)
-
-  /**
-   * Constructs an `Action` with default content.
-   */
-  final def apply(params: Attribute[_]*)(block: StackActionRequest[AnyContent] => Result): Action[AnyContent] =
-    apply(BodyParsers.parse.default, params: _*)(block)
 
   /**
    * Constructs an `Action`.
    */
-  final def apply[A](bodyParser: BodyParser[A], params: Attribute[_]*)(block: StackActionRequest[A] => Result): Action[A] =
-    async(bodyParser, params: _*) { request: StackActionRequest[A] =>
+  final def apply[A](bodyParser: BodyParser[A])(block: R[A] => Result): Action[A] =
+    async(bodyParser) { request: R[A] =>
       Future.successful(block(request))
     }
 
@@ -140,33 +136,19 @@ trait StackActionBuilder extends StackActionFunction {
     async(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent))(_ => block)
 
   /**
-   * Constructs an `Action` that returns a future of a result,
-   * with default content, and no request parameter.
-   */
-  final def async(params: Attribute[_]*)(block: => Future[Result]): Action[AnyContent] =
-    async(BodyParsers.parse.ignore(AnyContentAsEmpty: AnyContent), params: _*)(_ => block)
-
-  /**
    * Constructs an `Action` that returns a future of a result, with default content
    */
-  final def async(block: StackActionRequest[AnyContent] => Future[Result]): Action[AnyContent] =
+  final def async(block: R[AnyContent] => Future[Result]): Action[AnyContent] =
     async(BodyParsers.parse.default)(block)
 
   /**
    * Constructs an `Action` that returns a future of a result, with default content.
    */
-  final def async(params: Attribute[_]*)(block: StackActionRequest[AnyContent] => Future[Result]): Action[AnyContent] =
-    async(BodyParsers.parse.default, params: _*)(block)
-
-  /**
-   * Constructs an `Action` that returns a future of a result, with default content.
-   */
-  final def async[A](bodyParser: BodyParser[A], params: Attribute[_]*)(block: StackActionRequest[A] => Future[Result]): Action[A] =
+  final def async[A](bodyParser: BodyParser[A])(block: R[A] => Future[Result]): Action[A] =
     composeAction(new Action[A] {
       def parser = composeParser(bodyParser)
       def apply(request: Request[A]) = try {
-        val attrs = new TrieMap[AttributeKey[_], Any] ++= params.map(_.toTuple)
-        invokeBlock(StackActionRequest(request, attrs), block)
+        invokeBlock(StackActionRequest(request), block)
       } catch {
         // NotImplementedError is not caught by NonFatal, wrap it
         case e: NotImplementedError => throw new RuntimeException(e)
@@ -175,7 +157,6 @@ trait StackActionBuilder extends StackActionFunction {
       }
       override def executionContext = StackActionBuilder.this.executionContext
     })
-
 
   /**
    * Compose the parser.  This allows the action builder to potentially intercept requests before they are parsed.
@@ -193,12 +174,36 @@ trait StackActionBuilder extends StackActionFunction {
    */
   protected def composeAction[A](action: Action[A]): Action[A] = action
 
-  override def andThen[Q[_]](other: ActionFunction[StackActionRequest, Q]): ActionBuilder[Q] = new ActionBuilder[Q] {
-    def invokeBlock[A](request: Request[A], block: Q[A] => Future[Result]) =
-      self.invokeBlock[A](StackActionRequest(request), other.invokeBlock[A](_, block))
+  final def apply(params: Attribute[_]*): StackActionBuilder[R] = new StackActionBuilder[R] {
+    def invokeBlock[A](request: StackActionRequest[A], block: R[A] => Future[Result]) = {
+      val attrs = new TrieMap[AttributeKey[_], Any] ++= params.map(_.toTuple)
+      self.invokeBlock[A](request ++= attrs, block)
+    }
     override protected def composeParser[A](bodyParser: BodyParser[A]): BodyParser[A] = self.composeParser(bodyParser)
     override protected def composeAction[A](action: Action[A]): Action[A] = self.composeAction(action)
   }
+
+  /**
+   * Compose another ActionFunction with this one, with this one applied last.
+   */
+  override def andThen[Q[_]](other: ActionFunction[R, Q]): StackActionBuilder[Q] = new StackActionBuilder[Q] {
+    def invokeBlock[A](request: StackActionRequest[A], block: Q[A] => Future[Result]) =
+      self.invokeBlock[A](request, other.invokeBlock[A](_, block))
+    override protected def composeParser[A](bodyParser: BodyParser[A]): BodyParser[A] = self.composeParser(bodyParser)
+    override protected def composeAction[A](action: Action[A]): Action[A] = self.composeAction(action)
+  }
+}
+
+// Statck Action Function
+//~~~~~~~~~~~~~~~~~~~~~~~~~
+sealed trait StackActionFunctionHelper extends ActionFunction[StackActionRequest, StackActionRequest] with StackActionInjector { self =>
+  final def apply(params: Attribute[_]*): ActionFunction[StackActionRequest, StackActionRequest] =
+    new ActionFunction[StackActionRequest, StackActionRequest] {
+      def invokeBlock[A](request: StackActionRequest[A], block: StackActionRequest[A] => Future[Result]) = {
+        val attrs = new TrieMap[AttributeKey[_], Any] ++= params.map(_.toTuple)
+        self.invokeBlock[A](request ++= attrs, block)
+      }
+    }
 }
 
 /**
@@ -207,16 +212,18 @@ trait StackActionBuilder extends StackActionFunction {
  * its Action block with a parameter.
  * The critical (abstract) function is refine.
  */
-trait StackActionRefiner extends StackActionFunction
-    with ActionRefiner[StackActionRequest, StackActionRequest]
+trait StackActionRefiner
+    extends ActionRefiner[StackActionRequest, StackActionRequest]
+    with StackActionFunctionHelper
 
 /**
  * A simple kind of ActionRefiner which, given a request,
  * unconditionally transforms it to a new parameter type to be passed to
  * its Action block.  The critical (abstract) function is transform.
  */
-trait StackActionTransformer extends StackActionFunction
-    with ActionTransformer[StackActionRequest, StackActionRequest]
+trait StackActionTransformer
+    extends ActionTransformer[StackActionRequest, StackActionRequest]
+    with StackActionFunctionHelper
 
 /**
  * A simple kind of ActionRefiner which, given a request, may
@@ -224,5 +231,6 @@ trait StackActionTransformer extends StackActionFunction
  * continue its Action block with the same request.
  * The critical (abstract) function is filter.
  */
-trait StackActionFilter extends StackActionFunction
-    with ActionRefiner[StackActionRequest, StackActionRequest]
+trait StackActionFilter
+    extends ActionRefiner[StackActionRequest, StackActionRequest]
+    with StackActionFunctionHelper
