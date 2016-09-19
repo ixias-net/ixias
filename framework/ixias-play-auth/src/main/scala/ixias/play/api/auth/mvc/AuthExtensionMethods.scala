1982691efcad7db7a00128f6932e0ba904bc0a4a
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2016-09-19 23:41:44 +0900

    拡張関数定義を整理

---
+++ framework/ixias-play-auth/src/main/scala/ixias/play/api/auth/mvc/AuthExtensionMethods.scala
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the IxiaS services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package ixias.play.api.auth.mvc
+
+trait AuthExtensionMethods {
+  val Authenticated      = ixias.play.api.auth.mvc.Authenticated
+  val AuthenticatedOrNot = ixias.play.api.auth.mvc.AuthenticatedOrNot
+  val Authorized         = ixias.play.api.auth.mvc.Authorized
+}
