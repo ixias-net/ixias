a6a1b5ec17b9589f8d38287aa13a46074c543842
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2016-12-13 16:04:10 +0900

    フォーマット定義を揃える

--- framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickColumnTypeOps.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickColumnTypeOps.scala
@@ -37,32 +37,26 @@ sealed case class SlickColumnTypesExtension[P <: JdbcProfile](val driver: P)
   }
 
   /** [[org.joda.time.LocalDate]] */
-  object JodaLocalDateType extends driver.DriverJdbcType[LocalDate] {
+  object JodaLocalDate {
     type T1 = org.joda.time.LocalDate
     type T2 = java.sql.Date
 
-    protected def toT1(v: T2): T1 = v match {
-      case null => null
-      case date => new LocalDate(date.getTime)
+    protected def toT1(v: T2): T1 = if (v == null) null else new T1(v.getTime)
+    protected def toT2(v: T1): T2 = if (v == null) null else new T2(v.toDate.getTime)
 
+    object Type extends driver.DriverJdbcType[T1] {
+      def sqlType = java.sql.Types.DATE
+      def    getValue(       r: ResultSet,         idx: Int): T1   = toT1(r.getDate(idx))
+      def updateValue(v: T1, r: ResultSet,         idx: Int): Unit = r.updateDate(idx, toT2(v))
+      def    setValue(v: T1, p: PreparedStatement, idx: Int): Unit = p.setDate(idx, toT2(v))
+      override def valueToSQLLiteral(value: T1): String = "{d '" + toT2(value).toString + "'}"
     }
-    protected def toT2(v: T1): T2 = v match {
-      case null => null
-      case date => new java.sql.Date(date.toDate.getTime)
-    }
-
-    def sqlType = java.sql.Types.DATE
-    def    getValue(       r: ResultSet,         idx: Int): T1   = toT1(r.getDate(idx))
-    def updateValue(v: T1, r: ResultSet,         idx: Int): Unit = r.updateDate(idx, toT2(v))
-    def    setValue(v: T1, p: PreparedStatement, idx: Int): Unit = p.setDate(idx, toT2(v))
-    override def valueToSQLLiteral(value: T1): String = "{d '" + toT2(value).toString + "'}"
   }
-
 }
 
 trait SlickColumnTypeOps[P <: JdbcProfile] {
   val driver: P
   val columnTypes = SlickColumnTypesExtension(driver)
   implicit val jodaDateTimeColumnType  = columnTypes.JodaDateTime.Type
-  implicit val jodaLocalDateColumnType = columnTypes.JodaLocalDateType
+  implicit val jodaLocalDateColumnType = columnTypes.JodaLocalDate.Type
 }
