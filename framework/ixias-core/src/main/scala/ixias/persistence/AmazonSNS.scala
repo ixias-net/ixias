12250fdab24fe9ba2eec4110228ce78d5a2eaf73
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2017-04-30 18:03:58 +0900

    AmazonSNSの送信ログを記録する

--- framework/ixias-core/src/main/scala/ixias/persistence/AmazonSNS.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/AmazonSNS.scala
@@ -12,10 +12,11 @@ import ixias.persistence.model.DataSourceName
 import ixias.persistence.lifted.Aliases
 import ixias.persistence.backend.AmazonSNSBackend
 import ixias.persistence.dbio.Execution
+import ixias.util.Logging
 
 // Amazon SNS
 //~~~~~~~~~~~~
-trait AmazonSNS extends Aliases {
+trait AmazonSNS extends Aliases with Logging {
 
   // --[ Typedefs ]-------------------------------------------------------------
   type PublishResult = com.amazonaws.services.sns.model.PublishResult
@@ -37,14 +38,20 @@ trait AmazonSNS extends Aliases {
   /**
    * Sends a message to a topic's subscribed endpoints.
    */
-  def publish(message: String): Future[Seq[PublishResult]] =
+  def publish(message: String): Future[Seq[PublishResult]] = {
     backend.isSkip(dsn) match {
       case true  => Future.successful(Seq.empty)
       case false => for {
         client   <- backend.getDatabase(dsn)
         topicSeq <- Future.fromTry(backend.getTopicARN(dsn))
       } yield topicSeq map {
-        topic => client.publish(topic, message)
+        topic => {
+          val msgFmt = "AWS-SNS :: publish a message. topic = %s, message = %s, result = %s"
+          val result = client.publish(topic, message)
+          logger.info(msgFmt.format(topic, message, result.toString()))
+          result
+        }
       }
     }
+  }
 }
