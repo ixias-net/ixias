ea46ae8d52f5fa63aeaa5b89a2c646fab7abb762
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2017-06-22 10:53:37 +0900

    EntityのIDの所持状態を反映した EntityIO設計

--- framework/ixias-core/src/main/scala/ixias/persistence/dbio/EntityIOAction.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/dbio/EntityIOAction.scala
@@ -8,6 +8,7 @@
 package ixias.persistence.dbio
 
 import scala.concurrent.Future
+import scala.language.higherKinds
 import scala.concurrent.ExecutionContext
 import org.slf4j.LoggerFactory
 import ixias.util.Logger
@@ -16,13 +17,19 @@ import ixias.model.{ Tagged, Entity, IdStatus }
 /**
  * An Entity Action that can be executed on a persistence database.
  */
-trait EntityIOAction[K <: Tagged[_, _], E <: Entity[K, _]] extends IOAction {
+trait EntityIOAction[K <: Tagged[_, _], E[S <: IdStatus] <: Entity[K, S]] extends IOAction {
 
   /** The type of entity id */
-  type Id     = K
+  type Id = K
 
   /** The type of entity */
-  type Entity = E
+  type Entity[S <: IdStatus] = E[S]
+
+  /** The type of entity */
+  type EntityEmbeddedId = E[IdStatus.Exists]
+
+  /** The type of entity when it has not id. */
+  type EntityWithNoId   = E[IdStatus.Empty]
 
   /** The Execution Context */
   protected implicit val ctx: ExecutionContext = Execution.Implicits.trampoline
@@ -36,7 +43,7 @@ trait EntityIOAction[K <: Tagged[_, _], E <: Entity[K, _]] extends IOAction {
    * returned when a identity is not found The method implemented here throws an exception,
    * but it might be overridden in subclasses.
    */
-  def default(id: Id): Future[E] =
+  def default(id: Id): Future[EntityEmbeddedId] =
     Future.failed(new NoSuchElementException("identity not found: " + id))
 
   /**
@@ -44,7 +51,7 @@ trait EntityIOAction[K <: Tagged[_, _], E <: Entity[K, _]] extends IOAction {
    * This method invokes the `default` method of the map if there is no mapping
    * from the given identity to a value.
    */
-  def apply(id: Id): Future[E] =
+  def apply(id: Id): Future[EntityEmbeddedId] =
     get(id).flatMap(_ match {
       case Some(v) => Future.successful(v)
       case None    => default(id)
@@ -54,32 +61,32 @@ trait EntityIOAction[K <: Tagged[_, _], E <: Entity[K, _]] extends IOAction {
   /**
    * Optionally returns the value associated with a identity.
    */
-  def get(id: Id): Future[Option[E]]
+  def get(id: Id): Future[Option[EntityEmbeddedId]]
 
-  // /**
-  //  * Returns the value associated with a identity, or
-  //  * a default value if the identity is not contained in the repository.
-  //  */
-  // def getOrElse[E2 >: E](id: Id, f: Id => E2): Future[E2] =
-  //   get(id).map(_.getOrElse(f(id)))
-  // 
-  // /**
-  //  * Tests whether this repository contains a binding for a identity.
-  //  */
-  // def contains(id: Id): Future[Boolean] =
-  //   get(id).map(_.isDefined)
-  // 
-  // // --[ Methods ]--------------------------------------------------------------
-  // /**
-  //  * Adds a new identity/entity-value pair to this repository.
-  //  * If the map already contains a mapping for the identity,
-  //  * it will be overridden by the new value
-  //  */
-  // def store(entity: E): Future[Id]
-  // 
-  // /**
-  //  * Removes a identity from this map,
-  //  * returning the value associated previously with that identity as an option.
-  //  */
-  // def remove(id: Id): Future[Option[E]]
+  /**
+   * Returns the value associated with a identity, or
+   * a default value if the identity is not contained in the repository.
+   */
+  def getOrElse[E2 >: EntityEmbeddedId](id: Id, f: Id => E2): Future[E2] =
+    get(id).map(_.getOrElse(f(id)))
+
+  /**
+   * Tests whether this repository contains a binding for a identity.
+   */
+  def contains(id: Id): Future[Boolean] =
+    get(id).map(_.isDefined)
+
+  // --[ Methods ]--------------------------------------------------------------
+  /**
+   * Adds a new identity/entity-value pair to this repository.
+   * If the map already contains a mapping for the identity,
+   * it will be overridden by the new value
+   */
+  def store(entity: Entity[_]): Future[Id]
+
+  /**
+   * Removes a identity from this map,
+   * returning the value associated previously with that identity as an option.
+   */
+  def remove(id: Id): Future[Option[EntityEmbeddedId]]
 }
