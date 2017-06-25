a05ad1b880d2ae800a02620cef981ef456154bd7
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2017-06-26 00:20:44 +0900

    java.time.{ LocalDateTime, LocalDate, LocalTime }をslickに対応させる

--- framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickColumnTypeOps.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickColumnTypeOps.scala
@@ -8,26 +8,52 @@
 package ixias.persistence.lifted
 
 import slick.jdbc.JdbcProfile
-import java.sql.{ Timestamp, Date, Time }
-import org.joda.time.{ DateTime, LocalDate, LocalTime, DateTimeZone }
 
 trait SlickColumnTypeOps[P <: JdbcProfile] {
   val driver: P
   import driver.api._
 
+  // --[ Java8 Time ]-----------------------------------------------------------
+  // java.sql.Timestamp <-> java.time.LocalDateTime
+  implicit val javaLocalDateTimeColumnType =
+    MappedColumnType.base[java.time.LocalDateTime, java.sql.Timestamp](
+      dt => java.sql.Timestamp.valueOf(dt),
+      ts => ts.toLocalDateTime()
+    )
+
+  // java.sql.Date <-> java.time.LocalDate
+  implicit val javaLocalDateColumnType =
+    MappedColumnType.base[java.time.LocalDate, java.sql.Date](
+      ld => java.sql.Date.valueOf(ld),
+      d  => d.toLocalDate()
+    )
+
+  // java.sql.Time <-> java.time.LocalTime
+  implicit val javaLocalTimeColumnType =
+    MappedColumnType.base[java.time.LocalTime, java.sql.Time](
+      lt => java.sql.Time.valueOf(lt),
+      t  => t.toLocalTime()
+    )
+
+  // --[ Joda Time ]------------------------------------------------------------
   // java.sql.Timestamp <-> org.joda.time.DateTime
-  implicit val jodaDateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
-    dt => new Timestamp(dt.getMillis),
-    ts => new DateTime(ts.getTime)
-  )
+  implicit val jodaDateTimeColumnType =
+    MappedColumnType.base[org.joda.time.DateTime, java.sql.Timestamp](
+      dt => new java.sql.Timestamp(dt.getMillis),
+      ts => new org.joda.time.DateTime(ts.getTime)
+    )
+
   // java.sql.Date <-> org.joda.time.LocalDate
-  implicit val jodaLocalDateColumnType = MappedColumnType.base[LocalDate, Date](
-    ld => new Date(ld.toDateTimeAtStartOfDay(DateTimeZone.UTC).getMillis),
-    d  => new LocalDate(d.getTime)
-  )
+  implicit val jodaLocalDateColumnType =
+    MappedColumnType.base[org.joda.time.LocalDate, java.sql.Date](
+      ld => new java.sql.Date(ld.toDateTimeAtStartOfDay(org.joda.time.DateTimeZone.UTC).getMillis),
+      d  => new org.joda.time.LocalDate(d.getTime)
+    )
+
   // java.sql.Time <-> org.joda.time.LocalTime
-  implicit val jodaLocalTimeColumnType = MappedColumnType.base[LocalTime, Time](
-    lt => new Time(lt.toDateTimeToday.getMillis),
-    t  => new LocalTime(t, DateTimeZone.UTC)
-  )
+  implicit val jodaLocalTimeColumnType =
+    MappedColumnType.base[org.joda.time.LocalTime, java.sql.Time](
+      lt => new java.sql.Time(lt.toDateTimeToday.getMillis),
+      t  => new org.joda.time.LocalTime(t, org.joda.time.DateTimeZone.UTC)
+    )
 }
