6d0f974b5f06faa743919735ea94b74a10f3d1a5
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2017-06-28 01:06:14 +0900

    slick.lifted.Rep[Option[T @@ _]] の変換対応

--- framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickRepOps.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickRepOps.scala
@@ -21,4 +21,11 @@ trait SlickRepOps[P <: JdbcProfile]
   implicit def       toLongSlickRep[T](v:       Long @@ T): slick.lifted.Rep[Long]       = the[Identity[Long       @@ T]].unwrap(v)
   implicit def toBigDecimalSlickRep[T](v: BigDecimal @@ T): slick.lifted.Rep[BigDecimal] = the[Identity[BigDecimal @@ T]].unwrap(v)
   implicit def     toStringSlickRep[T](v:     String @@ T): slick.lifted.Rep[String]     = the[Identity[String     @@ T]].unwrap(v)
+
+  implicit def       toByteSlickRep[T](v: Option[      Byte @@ T]): slick.lifted.Rep[Option[Byte]]       = v.map(the[Identity[Byte       @@ T]].unwrap)
+  implicit def      toShortSlickRep[T](v: Option[     Short @@ T]): slick.lifted.Rep[Option[Short]]      = v.map(the[Identity[Short      @@ T]].unwrap)
+  implicit def        toIntSlickRep[T](v: Option[       Int @@ T]): slick.lifted.Rep[Option[Int]]        = v.map(the[Identity[Int        @@ T]].unwrap)
+  implicit def       toLongSlickRep[T](v: Option[      Long @@ T]): slick.lifted.Rep[Option[Long]]       = v.map(the[Identity[Long       @@ T]].unwrap)
+  implicit def toBigDecimalSlickRep[T](v: Option[BigDecimal @@ T]): slick.lifted.Rep[Option[BigDecimal]] = v.map(the[Identity[BigDecimal @@ T]].unwrap)
+  implicit def     toStringSlickRep[T](v: Option[    String @@ T]): slick.lifted.Rep[Option[String]]     = v.map(the[Identity[String     @@ T]].unwrap)
 }
