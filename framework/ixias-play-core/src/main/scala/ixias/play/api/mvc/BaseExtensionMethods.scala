9e5c17cb4028887e234676313a327a2dbba54025
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2017-07-01 19:10:25 +0900

    RequestHeaderのTypedKey取得ヘルパーを追加

--- framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/BaseExtensionMethods.scala
+++ framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/BaseExtensionMethods.scala
@@ -7,8 +7,14 @@
 
 package ixias.play.api.mvc
 
+import play.api.mvc.Result
+import scala.language.implicitConversions
+
 trait BaseExtensionMethods {
   val Cursor     = ixias.persistence.model.Cursor
   val JsonHelper = ixias.play.api.mvc.JsonHelper
   val FormHelper = ixias.play.api.mvc.FormHelper
+
+  implicit def convEitherToResult(v: Either[Result, Result]): Result =
+    v match { case Right(r) => r case Left(l) => l }
 }
