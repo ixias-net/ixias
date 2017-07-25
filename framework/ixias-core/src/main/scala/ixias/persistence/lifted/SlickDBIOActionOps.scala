3299c58c5bbecd626ca77c509311cb566b5a5bfb
Yoshinobu Kinugasa <yoshinobu.kinugasa@gmail.com>
2017-07-26 00:34:20 +0900

    Entityへの変換関数

--- framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickDBIOActionOps.scala
+++ framework/ixias-core/src/main/scala/ixias/persistence/lifted/SlickDBIOActionOps.scala
@@ -11,12 +11,38 @@ import ixias.model._
 import slick.dbio.{ DBIOAction, NoStream, Effect }
 import scala.concurrent.ExecutionContext
 import scala.language.implicitConversions
+import scala.reflect.ClassTag
 
 final case class SlickDBIOActionTransformer[K <: @@[_, _], M <: EntityModel[K], E <: Effect]
-  (val self: DBIOAction[Seq[M], NoStream, E]) extends AnyVal
+  (val self: DBIOAction[_, NoStream, E]) extends AnyVal
 {
-  def toEntitySeq(implicit ex: ExecutionContext): DBIOAction[Seq[Entity.EmbeddedId[K, M]], NoStream, E] =
-    self.map(_.map(Entity.EmbeddedId[K, M](_)))
+  // Option[M] => Option[Entity.EmbeddedId[K, M]]
+  def toEntityOption(implicit ctag: ClassTag[M], ex: ExecutionContext):
+      DBIOAction[Option[Entity.EmbeddedId[K, M]], NoStream, E] = self collect {
+    case Some(m) if ctag.runtimeClass.isInstance(m) =>
+      Some(Entity.EmbeddedId[K, M](m.asInstanceOf[M]))
+  }
+
+  // Option[M] => Option[R2]
+  def mapEntityOption[R2](fn: Entity.EmbeddedId[K, M] => R2)(implicit ctag: ClassTag[M], ex: ExecutionContext):
+      DBIOAction[Option[R2], NoStream, E] = self collect {
+    case Some(m) if ctag.runtimeClass.isInstance(m) =>
+      Some(fn(Entity.EmbeddedId[K, M](m.asInstanceOf[M])))
+  }
+
+  // Seq[M] => Seq[Entity.EmbeddedId[K, M]]
+  def toEntitySeq(implicit ctag: ClassTag[Seq[M]], ex: ExecutionContext):
+      DBIOAction[Seq[Entity.EmbeddedId[K, M]], NoStream, E] = self collect {
+    case itr if ctag.runtimeClass.isInstance(itr) =>
+      itr.asInstanceOf[Seq[M]].map(Entity.EmbeddedId[K, M](_))
+  }
+
+  // Seq[M] => Seq[R2]
+  def mapEntitySeq[R2](fn: Entity.EmbeddedId[K, M] => R2)(implicit ctag: ClassTag[Seq[M]], ex: ExecutionContext):
+      DBIOAction[Seq[R2], NoStream, E] = self collect {
+    case itr if ctag.runtimeClass.isInstance(itr) =>
+      itr.asInstanceOf[Seq[M]].map(m => fn(Entity.EmbeddedId[K, M](m)))
+  }
 }
 
 trait SlickDBIOActionOps[K <: @@[_, _], M <: EntityModel[K]] {
