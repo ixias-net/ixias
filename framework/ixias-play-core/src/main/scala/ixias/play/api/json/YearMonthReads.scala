68a743d500973ecf284b1aa85a468a519fa4e50a
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2018-01-09 00:09:31 +0900

    play-json で java.time.YearMonth が扱えるように Reads/Writes を実装

---
+++ framework/ixias-play-core/src/main/scala/ixias/play/api/json/YearMonthReads.scala
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the ixias services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package ixias.play.api.json
+
+import java.time.{ YearMonth, LocalDate, Instant, Clock, ZoneOffset }
+import java.time.temporal.UnsupportedTemporalTypeException
+import java.time.format.{ DateTimeFormatter, DateTimeParseException }
+import play.api.libs.json._
+import scala.language.implicitConversions
+
+object YearMonthReads extends EnvReads {
+
+  /** Instance of year-month based on formatter. */
+  implicit def YearMonthFormatterParser(formatter: DateTimeFormatter): TemporalParser[YearMonth] =
+    new TemporalParser[YearMonth] {
+      def parse(input: String): Option[YearMonth] = try {
+        Some(YearMonth.parse(input, formatter))
+      } catch {
+        case _: DateTimeParseException => None
+        case _: UnsupportedTemporalTypeException => None
+      }
+    }
+
+  /**
+   * Reads for the `java.time.YearMonth` type.
+   */
+  def yearMonthReads[T](parsing: T, corrector: String => String = identity)(implicit p: T => TemporalParser[YearMonth]): Reads[YearMonth] =
+    new Reads[YearMonth] {
+      def reads(json: JsValue): JsResult[YearMonth] = json match {
+        case JsNumber(d) => JsSuccess(YearMonth.from(epoch(d.toLong)))
+        case JsString(s) => p(parsing).parse(corrector(s)) match {
+          case Some(d) => JsSuccess(d)
+          case _ => JsError(Seq(JsPath ->
+            Seq(JsonValidationError("error.expected.date.isoformat", parsing))))
+        }
+        case _ => JsError(Seq(JsPath ->
+          Seq(JsonValidationError("error.expected.date"))))
+      }
+
+      @inline def epoch(millis: Long): LocalDate = LocalDate.now(
+        Clock.fixed(Instant.ofEpochMilli(millis), ZoneOffset.UTC)
+      )
+    }
+
+  /**
+   * The default typeclass to reads `java.time.YearMonth` from JSON.
+   */
+  implicit val DefaultYearMonthReads =
+    yearMonthReads(DateTimeFormatter.ofPattern("yyyy-MM"))
+}
---
+++ framework/ixias-play-core/src/main/scala/ixias/play/api/json/YearMonthWrites.scala
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the ixias services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package ixias.play.api.json
+
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import play.api.libs.json.EnvWrites
+import scala.language.implicitConversions
+
+object YearMonthWrites extends EnvWrites {
+
+  /** Formatting */
+  implicit def DefaultYearMonthFormatter(formatter: DateTimeFormatter) =
+    new TemporalFormatter[YearMonth] {
+      def format(temporal: YearMonth): String = {
+        formatter.format(temporal)
+      }
+    }
+
+  /**
+   * The default typeclass to write a `java.time.YearMonth`,
+   */
+  implicit val writesYearMonth =
+    temporalWrites[YearMonth, DateTimeFormatter](
+      DateTimeFormatter.ofPattern("yyyy-MM")
+    )
+}
