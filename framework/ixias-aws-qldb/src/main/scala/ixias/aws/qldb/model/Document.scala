2d3a954ae2fe54cd08945f60591f0e420ba60345
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2019-10-18 14:15:47 +0900

    Detecting conversion error to Nothing type at compile time.

---
+++ framework/ixias-aws-qldb/src/main/scala/ixias/aws/qldb/model/Document.scala
@@ -0,0 +1,13 @@
+/*
+ * Copyright ixias.net All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package ixias.aws.qldb.model
+
+case class Document(
+  documentId: String
+)
