1f210b46eb8cf54385e4364ffcdcd9a93b07d5c7
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2021-09-26 23:02:33 +0900

    update cursor behavior

--- framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/binder/Cursor.scala
+++ framework/ixias-play-core/src/main/scala/ixias/play/api/mvc/binder/Cursor.scala
@@ -46,14 +46,13 @@ trait CursorBindable {
         v1 <- implicitly[QueryStringBindable[Long]]._bindOption(key + ".offset")
         v2 <- implicitly[QueryStringBindable[Long]]._bindOption(key + ".limit")
       } yield v1.filter(_ > 0) -> v2.filter(_ > 0) match {
-        case (Some(v1), Some(v2)) => Some(Cursor(v1, Some(v2)))
-        case (None,     Some(v2)) => Some(Cursor(0L, Some(v2)))
-        case (Some(v1), None)     => Some(Cursor(v1, Some(limitDefault.toLong)))
-        case _                    => None
+        case (Some(v1), Some(v2)) => Cursor(v1, Some(v2))
+        case (None,     Some(v2)) => Cursor(0L, Some(v2))
+        case (Some(v1), None)     => Cursor(v1, Some(limitDefault.toLong))
+        case _                    => Cursor(0L, Some(limitDefault.toLong))
       }) match {
-        case Left(v)          => Some(Left(v))
-        case Right(None)      => None
-        case Right(Some(cur)) => cur.limit.map(_ > limitMax) match {
+        case Left(v)      => Some(Left(v))
+        case Right(cur)   => cur.limit.map(_ > limitMax) match {
           case Some(true) => Some(Right(cur.copy(limit = Some(limitMax.toLong))))
           case _          => Some(Right(cur))
         }
